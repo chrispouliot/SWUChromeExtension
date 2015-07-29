2731d8345d4370aa411bb3ab0f04bf649a0305b1
Christophe Pouliot <cristophepoug@gmail.com>
2015-07-29 14:50:24 -0700

    HackDay chrome extension

---
+++ content_script.js
@@ -0,0 +1,67 @@
+walk(document.body);
+
+function walk(node) {
+	// This function Modified from http://bit.ly/1o47KcH
+	var child, next;
+
+	switch ( node.nodeType ) {
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) {
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function whereToAddSw(word) {
+	// Return index to add 'sw' to if a 'u' is 1st or 2nd letter
+	if (word.charAt(0) === 'u' || word.charAt(0) === 'U') {
+		return 0
+	} else if (word.charAt(1) === 'u' || word.charAt(1) === 'U') {
+		return 1
+	}
+	return -1
+}
+
+function readySw(word) {
+	if (word === word.toUpperCase() || word.substring(0, 3) === word.substring(0, 3).toUpperCase()) {
+		return 'SW';
+	} else if (word.charAt(0) === word.charAt(0).toUpperCase()) {
+			if (word.charAt(0) === 'U') {
+				return 'SW';
+			}
+		return 'Sw';
+	}
+	return 'sw';			
+}
+
+
+function handleText(textNode) {
+	var text = textNode.nodeValue;
+	var words = text.split(" ");
+
+	// Loop through all words. If word has 1st or2nd letter as 'u', swu-i-fy it
+	for (var wordIndex = 0; wordIndex < words.length; wordIndex++) {
+		// If first letter is a 'u', add 'sw'
+		if (whereToAddSw(words[wordIndex]) == 0) {
+			var addSw = readySw(words[wordIndex]);
+			text = text.replace(words[wordIndex], addSw + words[wordIndex]);
+
+		// If second letter is a 'u', remove first letter with 'sw'
+		} else if (whereToAddSw(words[wordIndex]) == 1) {
+			var addSw = readySw(words[wordIndex]);
+			text = text.replace(words[wordIndex], addSw + words[wordIndex].substring(1));
+		}
+	}
+	
+	textNode.nodeValue = text;
+}
