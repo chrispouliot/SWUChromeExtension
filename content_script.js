a3a94f4c1d8ac3d21cc090abe55b77c9b1f34acf
Christophe Pouliot <cristophepoug@gmail.com>
2015-07-30 00:09:59 -0700

    better capitalization

--- content_script.js
+++ content_script.js
@@ -25,23 +25,30 @@ function walk(node) {
 function whereToAddSw(word) {
 	// Return index to add 'sw' to if a 'u' is 1st or 2nd letter
 	if (word.charAt(0) === 'u' || word.charAt(0) === 'U') {
-		return 0
+		return 0;
 	} else if (word.charAt(1) === 'u' || word.charAt(1) === 'U') {
-		return 1
+		return 1;
 	}
-	return -1
+	return -1;
 }
 
 function readySw(word) {
+	// URL -> SWURL
 	if (word === word.toUpperCase() || word.substring(0, 3) === word.substring(0, 3).toUpperCase()) {
-		return 'SW';
+		return 'SW'+word;
 	} else if (word.charAt(0) === word.charAt(0).toUpperCase()) {
+			// Update -> Swupdate
 			if (word.charAt(0) === 'U') {
-				return 'SW';
+				return 'Swu'+word.substring(1);
 			}
-		return 'Sw';
+		// Customer -> Swustomer
+		return 'Sw'+word.substring(1);
+	// customer -> swustomer
+	} else if (word.charAt(0) !== 'u') {
+		return 'sw'+word.substring(1);
 	}
-	return 'sw';			
+	// update -> swupdate
+	return 'sw'+word;			
 }
 
 
@@ -51,15 +58,19 @@ function handleText(textNode) {
 
 	// Loop through all words. If word has 1st or2nd letter as 'u', swu-i-fy it
 	for (var wordIndex = 0; wordIndex < words.length; wordIndex++) {
+		// 'swur' looks weird
+		if (words[wordIndex].toUpperCase() === 'our'.toUpperCase()) {
+			continue;
+		}
 		// If first letter is a 'u', add 'sw'
 		if (whereToAddSw(words[wordIndex]) == 0) {
 			var addSw = readySw(words[wordIndex]);
-			text = text.replace(words[wordIndex], addSw + words[wordIndex]);
+			text = text.replace(words[wordIndex], addSw);
 
 		// If second letter is a 'u', remove first letter with 'sw'
 		} else if (whereToAddSw(words[wordIndex]) == 1) {
 			var addSw = readySw(words[wordIndex]);
-			text = text.replace(words[wordIndex], addSw + words[wordIndex].substring(1));
+			text = text.replace(words[wordIndex], addSw);
 		}
 	}
 	
